1974896f5c11ed22f4d7c6b114f38a65f94f6499
MariellyCuevas <contact.cuevasmarielly@gmail.com>
2024-01-26 08:10:37 -0400

    Modo prueba

---
+++ app/models/article.rb
@@ -0,0 +1,16 @@
+class Article < ApplicationRecord
+    has_many :comments, dependent: :destroy
+  
+    validates :title, presence: true
+    validates :body, presence: true, length: { minimum: 10 }
+
+    #Después de aquí se jode todo
+    # VALID_STATUSES = ['public', 'private', 'archived']
+
+    # validates :status, inclusion: { in: VALID_STATUSES }
+  
+    # def archived?
+    #   status == 'archived'
+    # end
+end
+  
---
+++ app/models/comment.rb
@@ -0,0 +1,12 @@
+class Comment < ApplicationRecord
+  belongs_to :article
+
+  # #A partir de aquí se jode
+  # VALID_STATUSES = ['public', 'private', 'archived']
+
+  # validates :status, inclusion: { in: VALID_STATUSES }
+
+  # def archived?
+  #   status == 'archived'
+  # end
+end
