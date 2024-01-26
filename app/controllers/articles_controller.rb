ea17b6e409ebe4a042ca9fbf703af1e3d8415cfa
MariellyCuevas <mariellycuevas7@gmail.com>
2024-01-26 15:19:04 -0400

    Se agrego Visible

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+
+  # http_basic_authenticate_with name: "dhh", password: "secret",
+  #  except: [:index, :show]
+
   def index
     @articles = Article.all
   end
@@ -46,6 +50,8 @@ class ArticlesController < ApplicationController
     def article_params
       params.require(:article).permit(:title, :body, :status)
     end
+
+    
 
     
   # private
